75276fa10629830a273919644529b1de3cc20c8f
rubenschaafsma <06001866@student.hhs.nl>
2016-03-23 16:17:21 +0100

    tekstaanpassing
    hallo is nu hallo ik ben Ruben

--- src/keygamep3/KeyGameP3.java
+++ src/keygamep3/KeyGameP3.java
@@ -15,7 +15,7 @@ public class KeyGameP3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hallo!");
+        System.out.println("Hallo ik ben Ruben!");
     }
     
 }
