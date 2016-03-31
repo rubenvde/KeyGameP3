c42ce318e78a3e74f8c4bcb936622519e6502c0e
ciCic <koraypoyraz1@gmail.com>
2016-03-31 19:08:12 +0200

    Frame, paneelLevel en paneelKnop is gelukt
    De CENTER gedeelte van de FRAME is paneelLevel en de SOUTH de RESET
    knop. Afhankelijk van de dimensie past de FRAME en paneelLevel hun
    afmetingen aan.

--- src/keygamep3/Level.java
+++ src/keygamep3/Level.java
@@ -15,10 +15,11 @@ import javax.swing.*;
  */
 public class Level extends JFrame{
     
-    private Dimensie dimensie;
-    private Speler speler;
     private final int VAK_BREEDTE = 64;
     private final int VAK_HOOGTE = 64;
+    
+    private Dimensie dimensie;
+    private Speler speler;
     
     private JPanel paneelKnoppen, paneelLevel;
     private JButton startKnop, resetKnop;
@@ -28,17 +29,18 @@ public class Level extends JFrame{
     private JLabel [][] testCells;
 
     public Level() {
-        dimensie = new Dimensie(2,5);
-        testCells = new JLabel[10][10];
+        dimensie = new Dimensie(8,5);
+        testCells = new JLabel[dimensie.getY()][dimensie.getX()];
         
-        setTitle("TestGrid");
-        setSize(VAK_BREEDTE*dimensie.getX(), VAK_HOOGTE*dimensie.getY());
-        setLayout(new GridLayout(dimensie.getY(), dimensie.getX()));
+        setTitle("LevelGrid");
+        setSize(VAK_BREEDTE*dimensie.getX(), (VAK_HOOGTE*dimensie.getY()) + 52);
+        setLayout(new BorderLayout());
         
         //speelVeld = new Veld[][];
+        
         creëerVeld();
-        //paneelKnoppen();
-        //maakPaneel();
+        paneelKnoppen();
+        maakPaneel();
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
@@ -46,26 +48,27 @@ public class Level extends JFrame{
     }
     
     private void creëerVeld(){
+        paneelLevel = new JPanel();
+        paneelLevel.setLayout(new GridLayout(dimensie.getY(), dimensie.getX(), 0, 0));
+        paneelLevel.setBackground(Color.BLACK);
+        
         for (int i = 0; i < dimensie.getY(); i++) {
             for (int j = 0; j < dimensie.getX(); j++) {
                 testCells[i][j] = new JLabel();
-                testCells[i][j].setIcon(new ImageIcon(getClass().getResource("grass.png")));
-                testCells[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                
-                add(testCells[i][j]);
+                testCells[i][j].setIcon(new ImageIcon(getClass().getResource("grass.png")));//<-- Test afbeelding
+                paneelLevel.add(testCells[i][j]);
             }
         }
     }
-        
-        // Inner class
+    
+    ///    
+    /// Inner class
         class knopActie implements ActionListener{
           
         @Override
         public void actionPerformed(ActionEvent event)
         {   
-            if(event.getSource() == startKnop){// StartKnop
-                //add(levels[0].getLevel(), BorderLayout.CENTER);//hier komt een level van de classe Level
-            }else if(event.getSource() == resetKnop){// resetKnop
+            if(event.getSource() == resetKnop){// resetKnop
                 //zodra een level is gemaakt kan het getest worden.
             }
             revalidate();
@@ -75,9 +78,6 @@ public class Level extends JFrame{
     
     private void paneelKnoppen()
     {
-        startKnop = new JButton("Start");
-        startKnop.addActionListener(new knopActie());
-   
         resetKnop = new JButton("Reset");
         resetKnop.addActionListener(new knopActie());
     }
@@ -86,11 +86,7 @@ public class Level extends JFrame{
     {   
         paneelKnoppen = new JPanel();
         paneelKnoppen.setBackground(Color.DARK_GRAY);
-        //paneelKnoppen.add(startKnop);
         paneelKnoppen.add(resetKnop);
-        
-        paneelLevel = new JPanel();
-        paneelLevel.setLayout(new GridLayout(dimensie.getY(),dimensie.getX()));
         
         add(paneelLevel, BorderLayout.CENTER);
         add(paneelKnoppen, BorderLayout.SOUTH);
@@ -112,7 +108,7 @@ public class Level extends JFrame{
         
     }
     
-    public static void main(String[] args) { // <-- Geen zorgen jongens dit is alleen voor mij :D
+    public static void main(String[] args) { // <-- Geen zorgen, dit is alleen voor mij :D
         
         Level level = new Level();
     }
