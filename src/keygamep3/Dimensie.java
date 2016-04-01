b560f302234587e117a1784a67dd2af11977fa37
ciCic <koraypoyraz1@gmail.com>
2016-04-01 11:13:41 +0200

    getters aangemaakt in Dimensie class

--- src/keygamep3/Dimensie.java
+++ src/keygamep3/Dimensie.java
@@ -21,7 +21,16 @@ public class Dimensie {
     public int getX() {
         return this.x;
     }
+    
     public int getY() {
         return this.y;
+    }
+    
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public void setY(int y){
+        this.y = y;
     }
 }
