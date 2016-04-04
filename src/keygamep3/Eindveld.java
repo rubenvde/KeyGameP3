b9e3e4b1e72084266aad3064b4f71a4fc7fded5e
rubenschaafsma <06001866@student.hhs.nl>
2016-04-04 16:05:43 +0200

    nog meer

---
+++ src/keygamep3/Eindveld.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keygamep3;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Eindveld extends SpelElement {
+    
+    private ImageIcon afbeelding;
+    private boolean isEindveld;
+    
+    public Eindveld() {
+        
+    }
+    
+    public boolean isEindveld() {
+        return true;
+        
+    }
+    
+    public ImageIcon getAfbeelding() {
+        this.afbeelding = new ImageIcon("Plaatjes/Eindveld.png");
+        return afbeelding;
+    }
+    
+    public boolean isToegankelijk(int pincode) {
+        return true;
+    }
+    
+}
